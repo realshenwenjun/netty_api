0af4d0f145d324718cc78b72ced11b463d2e4392
shenwenjun3d@163.com <shenwenjun3d@163.com>
2018-02-02 15:47:06 +0800

    启动脚本

--- bin/start.sh
+++ bin/start.sh
@@ -3,7 +3,7 @@
 LOG_ROOT="/home/web/logs"
 echo "LOG_ROOT=${LOG_ROOT}"
 
-JAVA_OPTS=" -Xmx256m -Xms128m  -Djava.io.tmpdir=${LOG_ROOT}"
+JAVA_OPTS=" -Xmx256m -Xms128m  -DLOG_ROOT=${LOG_ROOT}"
 
 touch ${LOG_ROOT}/out.log
 java ${JAVA_OPTS}  -jar ../build/netty_api.jar >> ${LOG_ROOT}/out.log &
